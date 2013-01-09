93904cafd94cf4be710d0a961fd9e6f5b622bfd8
CJ <chern.jie@mig33global.com>
2013-01-09 09:07:08 +0800

    Bug fixes for deploy.php, in the end it will not work because fetching fails due to unauthenticated ssh access;

--- deploy.php
+++ deploy.php
@@ -3,7 +3,8 @@
 if (empty($_POST) || empty($_POST['payload']))
 	exit('Go read <a href="https://confluence.atlassian.com/display/BITBUCKET/POST+Service+Management">this</a>');
 
-define('WEBROOT', '/var/www');
+define('WEBROOT', '/var/www/');
+define('LOG_COMMITS', TRUE);
 
 /**
 $_POST['payload'] = '
@@ -62,26 +63,31 @@ function run_exec($command, &$output = array(), &$return_var = 0)
 $payload = json_decode($_POST['payload']);
 
 $project = $payload->repository->slug;
-$hashes = array();
-foreach ($payload->commits as $commit)
-	$hashes[] = $commit->node;
-file_put_contents(
-	sprintf('%s-%s.hash', $project, implode('-', $hashes))
-	, $_POST['payload']
-);
+if (LOG_COMMITS)
+{
+	$hashes = array();
+	foreach ($payload->commits as $commit)
+		$hashes[] = $commit->node;
+	file_put_contents(
+		sprintf('%s-%s.hash', $project, implode('-', $hashes))
+		, $_POST['payload']
+	);
+}
 
 header('Content-Type: text/plain');
 
 $command = $output = $return_var = null;
-chdir(WEBROOT . $project);
 
-$output = run_exec('git fetch origin');
-$output = run_exec('git rev-list --left-right --count @{upstream}..master');
-list($upstream, $ahead) = explode("\t", $output);
+chdir(WEBROOT . $project);
+// error_log(run_exec('which git'));
+// error_log(run_exec('pwd'));
+// error_log(run_exec('whoami'));
+$output = run_exec('git fetch --verbose origin');
+error_log($output);
+$output = run_exec('git rev-list --left-right --count master..@{upstream}');
+list($ahead, $upstream) = explode("\t", $output);
 if ($upstream > 0)
 {
 	$output = run_exec('git rebase origin/master || (git stash save cronjob && git rebase origin/master && git stash pop)');
-	// error_log(run_exec('pwd'));
-	// error_log(run_exec('whoami'));
 	error_log($output);
 }
