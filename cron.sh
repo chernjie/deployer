816471eb4c4c5e692478a8e6df8ef5e2cd762a06
CJ <chern.jie@mig33global.com>
2013-05-03 05:02:13 +0000

    Write up crontask;

---
+++ cron.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for i in $(ls -d /var/www/*/.git | sed s[.git[[)
+do
+	cd $i &&
+		[ $(git ls-files -o *.hash | wc -l) -gt 0 ] &&
+		git fetch origin &&
+		(
+			git rebase origin/master ||
+				(
+					git stash save cronjob &&
+						git rebase origin/master &&
+						git stash pop
+				) ||
+				echo -n
+		) &&
+			chmod -R g+w . &&
+			chown -R apache:staff . &&
+			git ls-files -o *.hash | xargs -n1 -I@ mv @ /var/log/bitbucket &&
+			date --rfc-3339=seconds | xargs echo $i >> /var/log/bitbucket/cronjob.log
+done # bitbucket
