caff490382cfde09bbf00e2a8a10cc9d66bf3f8c
CJ <lim@chernjie.com>
2014-10-26 23:59:37 +0800

    Added github support on top of bitbucket

--- README.md
+++ README.md
@@ -1,27 +1,28 @@
 deployer
 ========
 
-Custom Git Deployer
+Simple Git Deployer
 
-How it works
-------------
-1. Utilizing BitBucket POST services, when someone push a commit to BitBucket, BitBucket will make a POST request to `www.yourdomain.com/deploy.php`
+#### How it works
+
+1. Utilizing [Github webhooks](https://developer.github.com/webhooks/) or [BitBucket POST services](https://confluence.atlassian.com/display/BITBUCKET/POST+hook+management)
 2. `deploy.php` will create a new `commit.hash`
 3. `cron.sh` will pick up `commit.hash` and rebase your git repository
 
-Setup
------
+#### Setup
 
-1. Add or symlink `deploy.php` to the `DocumentRoot` of your web directory, e.g.
+1. Login to Github/Bitbucket, and add `http://yourproject/deploy.php` to your web hook.
+
+2. Add or symlink `deploy.php` to the `DocumentRoot` of your web directory, e.g.
 `/var/www/project/deploy.php`
-\* Make sure that `/var/www/project` is a git repository
+ * Make sure that `/var/www/project` is a git repository
 
-2. Add this to your cron
-`*/1 * * * * /var/www/deployer/cron.sh`
+3. Add this to your cron
+`* * * * * /var/www/deployer/cron.sh`
 
-Alternatives
-------------
+#### Alternatives
 
+* Bruteforce Cron `* * * * * cd /var/www/project && git pull --rebase`
 * https://github.com/thephpdeveloper/Deployer
 * https://github.com/mislav/git-deploy
 * https://github.com/BrunoDeBarros/git-deploy-php
--- deploy.php
+++ deploy.php
@@ -50,6 +50,7 @@ class CommandLine
 {
 	const DEBUG = DEBUG;
 	protected $lastOutput = '';
+
 	/**
 	 * Return full output instead of just last line
 	 * @param string $command
@@ -65,39 +66,44 @@ class CommandLine
 	}
 }
 
-class BitBucketDeployer extends CommandLine
+abstract class Deployer extends CommandLine
 {
-	const WEBROOT = WEBROOT; // '/var/www/';
+	const WEBROOT     = WEBROOT; // '/var/www/';
 	const LOG_COMMITS = LOG_COMMITS; // TRUE;
 
-	public function __construct($BitBucketPayload)
+	public function __construct($payload)
 	{
-		$this->main($BitBucketPayload);
+		self::DEBUG && error_log($payload);
+		$this->payload = $payload;
+		$this->main();
 	}
+
+	protected function getPayload()
+	{
+		static $payload;
+		if (empty($payload))
+			$payload = json_decode($this->payload);
+		return $payload;
+	}
+	abstract protected function getProject();
+	abstract protected function getHashes();
 
 	/**
 	 * Main execution
-	 * @param string $payload json_encoded BitBucket payload
 	 */
-	public function main($BitBucketPayload)
+	public function main()
 	{
-		$payload = json_decode($BitBucketPayload);
-
-		$project = $payload->repository->slug;
 		if (self::LOG_COMMITS)
 		{
-			$hashes = array();
-			foreach ($payload->commits as $commit)
-				$hashes[] = $commit->node;
 			file_put_contents(
-				sprintf('%s-%s.hash', $project, implode('-', $hashes))
-				, $BitBucketPayload
+				sprintf('hook-%s-%s.hash', $this->getProject(), implode('-', $this->getHashes()))
+				, json_encode($this->payload)
 			);
 		}
 
 		header('Content-Type: text/plain');
 
-		chdir(self::WEBROOT . $project);
+		chdir(self::WEBROOT . $this->getProject());
 		// $this->run_exec('which git');
 		// $this->run_exec('pwd');
 		// $this->run_exec('whoami');
@@ -110,7 +116,38 @@ class BitBucketDeployer extends CommandLine
 		}
 	}
 }
-if (empty($_POST) || empty($_POST['payload']))
-	exit('Go read <a href="https://confluence.atlassian.com/display/BITBUCKET/POST+Service+Management">this</a>');
-else
+
+class BitBucketDeployer extends Deployer
+{
+	protected function getProject()
+	{
+		return $this->getPayload()->repository->slug;
+	}
+	protected function getHashes()
+	{
+		foreach ($this->getPayload()->commits as $commit)
+			$hashes[] = $commit->node;
+		return $hashes;
+	}
+}
+
+class GithubDeployer extends Deployer
+{
+	protected function getProject()
+	{
+		return $this->getPayload()->repository->name;
+	}
+	protected function getHashes()
+	{
+		foreach ($this->getPayload()->commits as $commit)
+			$hashes[] = $commit->id;
+		return $hashes;
+	}
+}
+
+if (! empty($_POST['payload']))
 	new BitBucketDeployer($_POST['payload']);
+else if (array_key_exists('HTTP_USER_AGENT', $_SERVER) && strpos($_SERVER['HTTP_USER_AGENT'], 'GitHub-Hookshot') === 0)
+	new GithubDeployer(file_get_contents("php://input"));
+else
+	exit('Go read <a href="https://confluence.atlassian.com/display/BITBUCKET/POST+Service+Management">this</a>');
