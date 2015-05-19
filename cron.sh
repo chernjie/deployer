1b5983469cfd1dc2f29daead9ccb09615435fbd0
CJ <lim@chernjie.com>
2015-05-20 01:49:52 +0800

    capture errors

--- cron.sh
+++ cron.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 for i in $(ls -d /var/www/*/.git | sed s[.git[[)
 do
+	date --rfc-3339=seconds | xargs echo $i
 	cd $i &&
 		[ $(git ls-files -o *.hash | wc -l) -gt 0 ] &&
 		git fetch origin &&
@@ -16,6 +17,5 @@ do
 		) &&
 			chmod -R g+w . &&
 			chown -R www-data:www-data . &&
-			git ls-files -o *.hash | xargs -n1 -I@ mv @ /var/log/bitbucket &&
-			date --rfc-3339=seconds | xargs echo $i >> /var/log/bitbucket/cronjob.log
-done # bitbucket
+			git ls-files -o *.hash | xargs -n1 -I@ mv @ /var/log/bitbucket
+done 2>&1 | tee -a /var/log/bitbucket/cronjob.log
