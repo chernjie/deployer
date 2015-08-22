92ae3d3427add1cc3ccb968dc142f477a18d68ab
CJ <lim@chernjie.com>
2015-08-23 02:02:33 +0800

    Hide slack hook URL from logs

--- bin/slack.sh
+++ bin/slack.sh
@@ -10,7 +10,7 @@ getSlackConfig() {
 }
 
 main () {
-  getSlackConfig || _error git config deployer.slack.incoming-webhook not found
+  getSlackConfig > /dev/null || _error git config deployer.slack.incoming-webhook not found
   local username=$1
   shift
   local text="$@"
